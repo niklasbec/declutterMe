bf816159d95ea52c19165b3e6fe4fe154f82a901
niklasbec <niklasbeckerr@gmail.com>
2020-04-13 11:59:39 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+/*global chrome*/
+
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="heading">Declutter me</h1>
+        <div className="buttons">
+        <a id="closeOne">1</a>
+        <a id="closeThree">3</a>
+        <a id="closeAll">All</a> 
+        </div>
+        <a className="openall" id="openAll">Open All</a>
+        <a className="clearStorage" id="clearStorage">Clear Storage</a>
+        </header>
     </div>
   );
 }
